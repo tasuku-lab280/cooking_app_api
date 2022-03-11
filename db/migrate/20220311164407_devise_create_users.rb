a61da584eebd1410fa76af71341e85616e6c0ebb
tasuku-lab280 <loweng280@gmail.com>
2022-03-12 02:05:18 +0900

    feat: db:reinstall実装

--- db/migrate/20220311164407_devise_create_users.rb
+++ db/migrate/20220311164407_devise_create_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
-  def change
+  def up
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -38,5 +38,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     add_index :users, %i[email reset_password_token], unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+  end
+
+  def down
+    drop_table :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_11_164407) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email", "reset_password_token"], name: "index_users_on_email_and_reset_password_token", unique: true
   end
 
 end
