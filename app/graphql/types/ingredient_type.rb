db263bb6fa2004c1edfeda1ffbe5dc5392754089
tasuku-lab280 <loweng280@gmail.com>
2022-05-01 13:51:27 +0900

    fix: type整理

--- app/graphql/types/ingredient_type.rb
+++ app/graphql/types/ingredient_type.rb
@@ -3,9 +3,12 @@
 module Types
   class IngredientType < Types::BaseObject
     field :id, ID, null: false
-    field :recipe_id, Integer, null: false
+    field :user_id, Integer, null: false
     field :name, String, null: false
     field :quantity, String, null: false
+    field :expired_on, GraphQL::Types::ISO8601Date, null: false
+    field :description, String
+    field :picture, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
